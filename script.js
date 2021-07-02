7b322e1069a6f622195ada437ef2fc08902ff3d6
joyho796 <56969406+joyho796@users.noreply.github.com>
2021-07-03 02:19:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("mobileNav").style.height = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("mobileNav").style.height = "0%";
+}
